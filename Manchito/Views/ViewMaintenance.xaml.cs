32f9b1909eb3ee2286aa46506a4128930edce433
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2023-03-02 14:18:25 -0600

    edición de vistas

--- Manchito/Views/ViewMaintenance.xaml.cs
+++ Manchito/Views/ViewMaintenance.xaml.cs
@@ -12,12 +12,32 @@ public partial class ViewMaintenance : ContentPage
 
     }
 
+	private async void ViewItem(object sender, EventArgs r)
+	{
+		try
+		{
+			await Navigation.PushAsync(new ViewItem());
+		}catch(Exception ex) {
+			await DisplayAlert("Error", $"Error {ex.Message}", null);
+		}
+	}
+
 
 	private async void AddItem(object sender, EventArgs e)
 	{
 		try
 		{
-			await Navigation.PushAsync(new AddItem());
+			string action = await DisplayActionSheet("¿Qué tipo de item deseas añadir?", "Cancelar", null, "Pararrayos", "Montaje", "Bajante", "Sistema Puesta a Tierra", "Unificaciones", "Supresores");
+
+			if (action != null)
+			{
+				if (!action.Equals("Cancelar"))
+				{
+					string result = await DisplayPromptAsync("Alias", "Digite el nombre del item a agregar");
+				}
+				
+			}
+			//await Navigation.PushAsync(new AddItem());
 		}catch (Exception ex)
 		{
 			await DisplayAlert("Error", $"Error {ex.Message}", null);
