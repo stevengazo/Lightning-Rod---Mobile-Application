baad8571b2904838139305f8ce0a8b679ec1fa3a
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2023-06-06 15:47:27 -0600

    redesign

--- Manchito/ViewModel/ViewCategoryViewModel.cs
+++ Manchito/ViewModel/ViewCategoryViewModel.cs
@@ -13,6 +13,9 @@ using System.IO;
 using static Java.Util.Jar.Attributes;
 using Microsoft.Maui.Platform;
 using static Android.Renderscripts.ScriptGroup;
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using System.Threading;
 
 namespace Manchito.ViewModel
 {
@@ -102,7 +105,7 @@ namespace Manchito.ViewModel
 				}
 			}
 		}
-		public ICommand TakePhotoCommand { get; private set; }
+		public ICommand TakePhotoCommand { get; private set; }	
 		public ICommand TakeVideoCommand { get; private set; }
 		public ICommand AppearingCommand { get; private set; }
 		public ICommand ShareItemCommand { get; private set; }
@@ -110,7 +113,7 @@ namespace Manchito.ViewModel
 		public ICommand AddItemCommand { get; private set; }
 		public ICommand RecordAudioItem { get; private set; }
 		public ICommand DeleteAudioCommand { get; private set; }
-		public ICommand PlayAudioCommand { get; private set; }
+		public ICommand PlayItemCommand { get; private set; }
 		#endregion
 		public ViewCategoryViewModel()
 		{
@@ -123,11 +126,23 @@ namespace Manchito.ViewModel
 			AppearingCommand = new AsyncRelayCommand(LoadCategory);
 			ShareItemCommand = new Command((O) => SharePhoto(O));
 			DeleteItemCommand = new Command((O) => DeletePhoto(O));
-			AddItemCommand = new AsyncRelayCommand( async()=> await AddPhotoFromGalleryAsync());
+            PlayItemCommand = new Command((O) => PlayAudio(O));
+            AddItemCommand = new AsyncRelayCommand( async()=> await AddPhotoFromGalleryAsync());
 			RecordAudioItem = new AsyncRelayCommand(async ()=> await RecordAudioAsync());
 			DeleteAudioCommand = new AsyncRelayCommand(async (o)=> await DeleteAudioAsync(o));			
 		}
-		private async Task DeleteAudioAsync(object Object)
+
+        private async void PlayAudio(object o)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            var toast = Toast.Make("Reproduciendo audio", ToastDuration.Long, 14);
+            await toast.Show(cancellationTokenSource.Token);
+            string path = o.ToString();
+			_audioPlayer.Play(path);
+            
+        }
+
+        private async Task DeleteAudioAsync(object Object)
 		{		
 			throw new NotImplementedException();
 		}
@@ -153,15 +168,22 @@ namespace Manchito.ViewModel
 							using (var db = new DBLocalContext())
 							{
 								AudioNote audioNote = new AudioNote();
-								audioNote.AudioNoteId = db.AudioNote.Select(A => A.AudioNoteId).FirstOrDefault() + 1;
+								audioNote.AudioNoteId = db.AudioNote.OrderByDescending(A=>A.AudioNoteId).Select(A => A.AudioNoteId).FirstOrDefault() + 1;
 								audioNote.PathFile = pathAudio;
 								audioNote.CategoryId = CategoryItem.CategoryId;
 								db.AudioNote.Add(audioNote);
+								db.SaveChanges();
 							}
+							await LoadAudio();
+                            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+                            var toast = Toast.Make("Audio Guardado",ToastDuration.Long,14);
+                            await toast.Show(cancellationTokenSource.Token);
                         }
 						else
 						{                                                      
-                            await Application.Current.MainPage.DisplayAlert("Error interno", "Error al guardar el audio", "OK");
+                               CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+                            var toast = Toast.Make("Error al guardar el audio",ToastDuration.Long,16);
+                            await toast.Show(cancellationTokenSource.Token);
                         }
 					}
 					else
@@ -189,7 +211,6 @@ namespace Manchito.ViewModel
 		{
 			var Response = Application.Current.MainPage.DisplayAlert("Alerta", "Deseas borrar este dato", "Yes", "No");
 		}
-
 
 		private async Task<int> GetLastIdAudio()
 		{
@@ -450,14 +471,21 @@ namespace Manchito.ViewModel
 						using FileStream localFileStream = File.OpenWrite(localFilePath);
 						await sourceStream.CopyToAsync(localFileStream);
 						await RegisterPhoto(localFilePath);
-						await loadImages();
-					}
+                        await loadImages();
+
+						 CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+                        var toast = Toast.Make("Foto Guardada", ToastDuration.Long, 14);
+                        await toast.Show(cancellationTokenSource.Token);
+
+                    }
 				}
 			}
 			catch (Exception ex)
 			{
-				await Application.Current.MainPage.DisplayAlert("Error TakePhotoAndroid ", $"Error: {ex.Message}", "ok");
-			}
+                CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+                var toast = Toast.Make("La foto no fue tomada", ToastDuration.Long, 14);
+                await toast.Show(cancellationTokenSource.Token);
+            }
 		}
 		
 	}
